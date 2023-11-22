3888e81ac6458e55306512b0470a64c0222b9534
20110656@student.hcmute.edu.vn <20110656@student.hcmute.edu.vn>
2023-11-23 03:37:05 +0700

    Update new template

---
+++ OnlineShop/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
