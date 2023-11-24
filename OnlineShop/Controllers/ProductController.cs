8364328dc9a35b9e5994353274b049c2adc6e472
NguyeThanhLoi <94297328+NTL212@users.noreply.github.com>
2023-11-24 13:20:00 +0700

    Update list product, search product, detail product

---
+++ OnlineShop/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using OnlineShop.Models;
+using OnlineShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
+
+namespace OnlineShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly OnlineShopContext _context;
+        public ProductController(ILogger<ProductController> logger, OnlineShopContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index(int? page)
+        {
+            var productList = _context.Products.Include(p => p.Category).Include(p => p.Style).ToPagedList(page ?? 1, 5);
+            //var categoryList = _context.Categories.ToList();
+            //ViewData["Categories"] = categoryList;
+            return View(productList);
+        }
+        [HttpPost]
+        public IActionResult Search(int? page, string keyword)
+        {
+            var results = _context.Products.Include(p => p.Category).Include(p => p.Style)
+            .Where(p => p.ProductName.Contains(keyword)).ToPagedList(page ?? 1, 5);
+            //var categoryList = _context.Categories.ToList();
+            //ViewData["Categories"] = categoryList;
+            return View("Index", results);
+        }
+        public IActionResult Detail(int id)
+        {
+            var product = _context.Products.Include(p => p.Category).Include(p => p.Style).FirstOrDefault(p=>p.ProductId.Equals(id));
+
+            if (product == null)
+            {
+                return NotFound(); // Trả về 404 nếu không tìm thấy sản phẩm
+            }
+
+            return View(product);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
